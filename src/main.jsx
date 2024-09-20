cc4730c0cfacfa898f919a0e780078d176c83b3f
OmarElmohamdy77 <omaremara68@gmail.com>
2024-09-20 03:12:46 +0300

    third commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -8,6 +9,8 @@ import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
